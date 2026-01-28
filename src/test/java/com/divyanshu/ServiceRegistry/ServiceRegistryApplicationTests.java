cb6cd359cc92452fcdf1e78e62e154fb1b817a4a
divyanshu <divyanshupunse3@gmail.com>
2026-01-28 12:13:32 +0530

    test commit

---
+++ src/test/java/com/divyanshu/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyanshu.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
